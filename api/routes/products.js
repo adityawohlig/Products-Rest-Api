4d40d38c8d792a1f1d462bd4cf13618a847d9a74
Aditya Bapat <adityawohlig@gmail.com>
2022-01-17 17:53:05 +0530

    Express router created

---
+++ api/routes/products.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+//  /products 
+router.get('/',(req,res, next) =>{
+    res.status(200).json({
+        message: 'Product route for Get handled successfully by router.'
+    })
+} );
+
+router.post('/',(req,res, next) =>{
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+
+    res.status(200).json({
+        message: 'Post request handled successfully by Product route.',
+        createdProduct: product
+    })
+});
+
+router.get('/:productId',(req,res, next) =>{
+    const id = req.params.productId;
+
+});
+
+router.patch('/:productId',(req,res, next) =>{
+    const id = req.params.productId;
+    
+});
+
+router.delete('/:productId',(req,res, next) =>{
+    const id = req.params.productId;
+    
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+// ----------------------------------------------------------------
+// morgan console logs the requests
+// ----------------------------------------------------------------
+
+
+// router being exported from respective module.exports, see files
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+// instead of body-parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(morgan('dev'));
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+// here request works for get, post etc, it is middleware
+
+
+// error handling
+// ----------------------------------------------------------------
+app.use(function(req, res, next) {
+    const error = new Error('Not Found the Page buddy');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            // most error have message property
+            message: error.message
+        }
+    })
+});
+
+// CORS Error Handling
+
+
+// ----------------------------------------------------------------
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: "It works!"
+//     });
+// });
+// ----------------------------------------------------------------
+// here app is not app.js 
+module.exports = app;
