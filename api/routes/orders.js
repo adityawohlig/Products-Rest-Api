9e939fdd69665a9438c9a1eab6bef6bf4f00bd5a
Aditya Bapat <adityawohlig@gmail.com>
2022-01-18 15:23:00 +0530

    controllers, jwt, login, auth middleware added.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,22 +1,18 @@
-const express = require('express');
+// 
+
+const express = require("express");
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
-router.get('/', function (req, res, next){
-    res.status(200).json({
-        message: 'Orders Fetched'
-    })
-});
+const OrdersController = require('../controllers/orders');
 
-router.post('/', function(req, res, next){
-    const order = {
-        productId : req.body.productId,
-        quantity : req.body.quantity
-    };
+// Handle incoming GET requests to /orders
+router.get("/", checkAuth, OrdersController.orders_get_all);
 
-    res.status(201).json({
-        message: 'Order is created successfully',
-        order: order
-    })
-})
+router.post("/", checkAuth, OrdersController.orders_create_order);
+
+router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
+
+router.delete("/:orderId", checkAuth, OrdersController.orders_delete_order);
 
 module.exports = router;
